89f4168c9d2eebd98569eb11b56920a883d64bfd
karan2808 <karantank97@gmail.com>
2021-05-13 18:09:02 -0400

    process/transform images

--- process_images.py
+++ process_images.py
@@ -1,82 +1,39 @@
 from sklearn.datasets import load_sample_image
 from sklearn.feature_extraction import image as image_transforms
+from sklearn.feature_extraction.image import extract_patches_2d
 import cv2
 import numpy as np
 
-def get_patches(image, patch_size):
-    '''Get N image overlapping patches with shape (patch_size, patch_size) and vectorize to (patch_size**2, N)'''
+def get_example_image(image_name='Barbara', image_shape=(512, 512), sigma=None, image_path=None): 
+    image = 0
+    # Load the image 
+    if image_name is not None:
+        try:
+            image = cv2.imread('example_images/' + str(image_name) + '.png', cv2.IMREAD_GRAYSCALE)
+        except:
+            print("Example Image Not Found")
+            return
+    else:
+        try:
+            image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+        except:
+            print("Path Not Found")
+            return
+    # Reshape the image
+    image = cv2.resize(image, image_shape).astype('float32')
     
-    # # Compute the selection tensor R
-    # indices = np.arange(image.size).reshape(image.shape)                                 
-    # patch_idx = image_transforms.extract_patches_2d(indices, (patch_size, patch_size))  # shape (num patches, patch_size, patch_size)
-    # patch_idx = patch_idx.reshape(-1, patch_size**2, 1)                                 # shape (num patches, patch_size*patch_size)
+    # Add noise to the image
+    if sigma != None:
+        image = image + np.random.normal(scale=sigma,
+                                    size=image.shape).astype(image.dtype)
+    
+    return image
 
-    # N_p = patch_idx.shape[0]   # number of patches
-    # N = image.size             # number of pixels in image
+def normalize(image):
+    return (image - np.amin(image)) / (np.amax(image) - np.amin(image))
 
-    # R = np.zeros((N_p, patch_size**2, N))  
-    # np.put_along_axis(R, patch_idx, 1, axis=-1)
-
-    # # Compute actual image patches from the selection matrices
-    # y = image.reshape(1,-1,1).repeat(N_p,axis=0)
-    # patches = R @ y
-
-    # patches = patches.reshape(patch_size**2, -1)
-
-    # return R, patches
-
-    patches = image_transforms.extract_patches_2d(image, (patch_size, patch_size))
-    return patches.reshape(patch_size ** 2, -1)
-
-def get_overcomplete_dictionary(n, K, normalized=True):
-    """
-    Builds a Dictionary matrix matrix using the inverse discrete cosine transform of type II,
-    cf. https://en.wikipedia.org/wiki/Discrete_cosine_transform#DCT-II
-    Args:
-        n: number of dictionary rows
-        K: number of dictionary columns
-        normalized: If True, the columns will be l2-normalized
-    Returns:
-        Dictionary build from the Kronecker-Delta of the inverse discrete cosine transform of type II applied to the identity.
-    """
-    D = np.zeros((K, n))
-    for i in range(n):
-        v = np.zeros(n)
-        v[i] = 1.0
-        y = np.array([sum(np.multiply(v, np.cos((0.5 + np.arange(n)) * k * np.pi / K))) for k in range(K)])
-        if normalized:
-            y[0] = 1 / np.sqrt(2) * y[0]
-            y = np.sqrt(2 / n) * y
-        D[:, i] = y
-    return np.kron(D.T, D.T)
-
-def visualize_dictionary(D, name='D.png'):
-    n, K = D.shape
-    M = D
-    # stretch atoms
-    for k in range(K):
-        M[:, k] = M[:, k] - (M[:, k].min())
-        if M[:, k].max():
-            M[:, k] = M[:, k] / D[:, k].max()
-
-    # patch size
-    n_r = int(np.sqrt(n))
-
-    # patches per row / column
-    K_r = int(np.sqrt(K))
-
-    # we need n_r*K_r+K_r+1 pixels in each direction
-    dim = n_r * K_r + K_r + 1
-    V = np.ones((dim, dim)) * np.min(D)
-
-    # compute the patches
-    patches = [np.reshape(D[:, i], (n_r, n_r)) for i in range(K)]
-
-    # place patches
-    for i in range(K_r):
-        for j in range(K_r):
-            V[j * n_r + 1 + j:(j + 1) * n_r + 1 + j, i * n_r + 1 + i:(i + 1) * n_r + 1 + i] = patches[
-                i * K_r + j]
-    V *= 255
-    cv2.imwrite(name, V.astype(np.uint8))
-    # cv2.waitKey(0)
+def get_Y(image, patch_size):
+    patches = extract_patches_2d(image, (patch_size, patch_size))
+    patches = [patch.reshape(patch_size**2) for patch in patches]
+    Y       = np.array(patches).T
+    return Y
