43963bd7a1e11e375083d997cd6de5293446451a
karan2808 <karantank97@gmail.com>
2021-05-14 00:23:15 -0400

    evaluation code

--- evaluate.py
+++ evaluate.py
@@ -1,8 +1,14 @@
 import cv2
-from metrics import psnr
+from metrics import psnr, rmse, structural_similarity
 
-original_image = cv2.imread('original_image.png')
-denoised_image = cv2.imread('denoised_image.png')
+original_image = cv2.imread('original_image.png').astype('float')
+denoised_image = cv2.imread('noisy_image.png').astype('float')
 
 psnr_ = psnr(original_image, denoised_image)
-print("The psnr is " + str(psnr_))
+print("The Peak Signal To Noise Ratio is " + str(psnr_))
+
+mse_ = rmse(original_image, denoised_image)
+print("The Root Mean Squared Error is " + str(mse_))
+
+ssim = structural_similarity(original_image, denoised_image)
+print("The structural similarity index is " + str(ssim))
