33b7d20c8c7ad292cb89f28e54416c4ac023b367
karan2808 <karantank97@gmail.com>
2021-05-14 00:24:04 -0400

    updated

--- main.py
+++ main.py
@@ -3,16 +3,30 @@ from process_images import get_example_image, normalize
 import matplotlib.pyplot as plt 
 from dictionaries import get_dictionary
 from denoiser import KSVDDenoiser
+import cv2
+from metrics import psnr
+import numpy as np 
 
 patch_size     = 8
-K              = 11
+K              = 16
+sigma          = 5
+iterations     = 5
 
-original_image = get_example_image('Barbara', image_shape=(256, 256))
-noisy_image    = get_example_image('Barbara', image_shape=(256, 256), sigma = 20)
+# original_image = get_example_image(image_path='example_images/CameraMan.png', image_shape=(256, 256))
+# noisy_image    = get_example_image(image_path='example_images/CameraMan.png', image_shape=(256, 256), sigma = sigma)
+
+original_image = get_example_image(image_name='Barbara', image_shape=(512, 512))
+noisy_image    = get_example_image(image_name='Barbara', image_shape=(512, 512), sigma = sigma)
 
 dict_          = get_dictionary(patch_size, K)
-denoiser       = KSVDDenoiser(patch_size = 8, iterations = 5, lambd = 0.5, sigma = 20, noise_gain = 1.15, viz_dict=False)
+# dict_          = np.random.normal(size = dict_.shape)
+
+denoiser       = KSVDDenoiser(patch_size = patch_size, iterations = iterations, lambd = 30/sigma, sigma = sigma, noise_gain = 1.15, viz_dict=True)
 denoised_img   = denoiser.denoise(noisy_image, dict_)
+
+cv2.imwrite('original_image.png', normalize(original_image)*255)
+cv2.imwrite('noisy_image.png', normalize(noisy_image)*255)
+cv2.imwrite('denoised_image.png', normalize(denoised_img)*255)
 
 plt.subplot(1, 3, 1)
 plt.title('original image')
